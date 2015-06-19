8eb9202dea81ab2acd38be3f80d06899f5bcc53f
Jeromy Evans (personal) <jeromy.evans@blueskyminds.com.au>
2015-06-19 15:33:39 +1000

    Code reformat

--- ios-oauth2-example/SuccessViewController.m
+++ ios-oauth2-example/SuccessViewController.m
@@ -20,7 +20,6 @@
 - (void)viewDidLoad {
   [super viewDidLoad];
   
-  
   [self makeInitialRequest];
 }
 
@@ -44,61 +43,65 @@
   NSArray* accounts = [[NXOAuth2AccountStore sharedStore] accountsWithAccountType:@"oauth-jwt-example"];
   
   for (NXOAuth2Account *account in accounts) {
-
+    
     [NXOAuth2Request performMethod:@"GET"
-                    onResource:[NSURL URLWithString:@"http://localhost:3000/"]
-               usingParameters:nil
-                   withAccount:account
-           sendProgressHandler:^(unsigned long long bytesSend, unsigned long long bytesTotal) {
-             // e.g., update a progress indicator
-           }
-               responseHandler:^(NSURLResponse *response, NSData *responseData, NSError *error){
-                 // Process the response
-                 NSString *responseString = [[NSString alloc] initWithData:responseData encoding:NSUTF8StringEncoding];
-                 
-                 NSLog(@"Respone: %@", responseString);
-                 
-                 dispatch_async(dispatch_get_main_queue(), ^{
-                   [self.textView setText:responseString];
-                 });
-               }];
+                        onResource:[NSURL URLWithString:@"http://localhost:3000/"]
+                   usingParameters:nil
+                       withAccount:account
+               sendProgressHandler:^(unsigned long long bytesSend, unsigned long long bytesTotal) {
+                 // e.g., update a progress indicator
+               }
+                   responseHandler:^(NSURLResponse *response, NSData *responseData, NSError *error){
+                     // Process the response
+                     NSString *responseString = [[NSString alloc] initWithData:responseData encoding:NSUTF8StringEncoding];
+                     
+                     NSLog(@"Respone: %@", responseString);
+                     
+                     dispatch_async(dispatch_get_main_queue(), ^{
+                       [self.textView setText:responseString];
+                     });
+                   }];
   }
  
 }
 
 // this version allows custom headers to be set
 - (void) performCustomGETOperation {
-   for (NXOAuth2Account *account in [[NXOAuth2AccountStore sharedStore] accountsWithAccountType:@"oauth-jwt-example"]) {
-      NXOAuth2Request *theRequest = [[NXOAuth2Request alloc] initWithResource:[NSURL URLWithString:@"http://localhost:3000/"]
-                                                                       method:@"GET"
-                                                                   parameters:nil];
-      theRequest.account = account;
   
-      NSMutableURLRequest *sigendRequest = [[theRequest signedURLRequest] mutableCopy];
+  NSArray* accounts = [[NXOAuth2AccountStore sharedStore] accountsWithAccountType:@"oauth-jwt-example"];
   
-      [sigendRequest setHTTPMethod:@"GET"];
-      [sigendRequest setValue:@"text/html,application/json" forHTTPHeaderField:@"Accept"];
-  //    [sigendRequest setValue:@"multipart/form-data" forHTTPHeaderField:@"Content-Type"];
-  
-      NXOAuth2Connection *connection = [[NXOAuth2Connection alloc]
-        initWithRequest:sigendRequest
-        requestParameters:nil
-        oauthClient:account.oauthClient
-        sendingProgressHandler:nil
-        responseHandler:^(NSURLResponse *response, NSData *responseData, NSError *error){
-  
-  //        NSLog(@"dataAsString %@", [NSString stringWithUTF8String:[responseData bytes]]);
-          NSString *responseString = [[NSString alloc] initWithData:responseData encoding:NSUTF8StringEncoding];
-  
-          NSLog(@"Respone: %@", responseString);
-  
-          dispatch_async(dispatch_get_main_queue(), ^{
-            [self.textView setText:responseString];
-          });
-        }];
-     //connection.delegate = self;  <NXOAuth2ConnectionDelegate>
-  
-   }
+  for (NXOAuth2Account *account in accounts) {
+    
+    NXOAuth2Request *theRequest = [[NXOAuth2Request alloc] initWithResource:[NSURL URLWithString:@"http://localhost:3000/"]
+                                                                     method:@"GET"
+                                                                 parameters:nil];
+    theRequest.account = account;
+    
+    NSMutableURLRequest *sigendRequest = [[theRequest signedURLRequest] mutableCopy];
+    
+    [sigendRequest setHTTPMethod:@"GET"];
+    [sigendRequest setValue:@"text/html,application/json" forHTTPHeaderField:@"Accept"];
+    //    [sigendRequest setValue:@"multipart/form-data" forHTTPHeaderField:@"Content-Type"];
+    
+    NXOAuth2Connection *connection = [[NXOAuth2Connection alloc]
+                                      initWithRequest:sigendRequest
+                                      requestParameters:nil
+                                      oauthClient:account.oauthClient
+                                      sendingProgressHandler:nil
+                                      responseHandler:^(NSURLResponse *response, NSData *responseData, NSError *error){
+                                        
+                                        //        NSLog(@"dataAsString %@", [NSString stringWithUTF8String:[responseData bytes]]);
+                                        NSString *responseString = [[NSString alloc] initWithData:responseData encoding:NSUTF8StringEncoding];
+                                        
+                                        NSLog(@"Respone: %@", responseString);
+                                        
+                                        dispatch_async(dispatch_get_main_queue(), ^{
+                                          [self.textView setText:responseString];
+                                        });
+                                      }];
+    //connection.delegate = self;  <NXOAuth2ConnectionDelegate>
+    
+  }
 }
 
 /*
